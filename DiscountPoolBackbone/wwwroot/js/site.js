87b8a8038cc08f9931c738875e7f3f93e9e9bdfb
Ustin <ustinx@mail.ru>
2020-03-04 22:02:41 +0300

    add temp uploader (only front-side)

--- DiscountPoolBackbone/wwwroot/js/site.js
+++ DiscountPoolBackbone/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    if (window.File && window.FileList && window.FileReader) {
+        $("#files").on("change", function (e) {
+            var files = e.target.files,
+                filesLength = files.length;
+            for (var i = 0; i < filesLength; i++) {
+                var f = files[i]
+                var fileReader = new FileReader();
+                fileReader.onload = (function (e) {
+                    var file = e.target;
+                    $("<span class=\"pip\">" +
+                        "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
+                        "<br/><span class=\"remove\">Remove image</span>" +
+                        "</span>").insertAfter("#files");
+                    $(".remove").click(function () {
+                        $(this).parent(".pip").remove();
+                    });
+                });
+                fileReader.readAsDataURL(f);
+            }
+        });
+    } else {
+        alert("Your browser doesn't support to File API")
+    }
+});
